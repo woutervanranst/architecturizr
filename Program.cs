6c832e80c100c4d4b1f68979629b12332ba237e1
Wouter Van Ranst <wouter.vanranst@gmail.com>
2022-06-22 06:44:22 +0200

    read General, Nodes tabs

---
+++ Program.cs
@@ -0,0 +1,6 @@
+﻿// See https://aka.ms/new-console-template for more information
+using architecturizr;
+
+Console.WriteLine("Hello, World!");
+
+var s = new SourceFile("/Users/wouter/Documents/GitLab/solution-architecture/c4/source2.xlsx");
---
+++ SourceFile.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using ExcelToEnumerable;
+
+namespace architecturizr;
+
+/// <summary>
+/// Read the Excel Source file
+/// https://stackoverflow.com/a/15793495
+/// </summary>
+public class SourceFile
+{
+
+    public SourceFile(string fileName)
+    {
+        var exceptionList = new List<Exception>();
+
+        // Parse General Tab
+        var generalRows = fileName.ExcelToEnumerable<General>(
+            x => x
+                .UsingSheet("General")
+                .UsingHeaderNames(false) //map using column numbers, not names
+                .OutputExceptionsTo(exceptionList)
+            );
+
+        if (exceptionList.Any())
+            throw new Exception();
+
+        Title = generalRows.Single(r => r.Key == "Title").Value;
+
+
+        // Parse Nodes Tab
+        var nodes = fileName.ExcelToEnumerable<Node>(
+            x => x
+                .UsingSheet("Nodes")
+                .OutputExceptionsTo(exceptionList)
+                .UsingHeaderNames(false) //map using column numbers, not names
+                .StartingFromRow(3) //data as of row 3
+            );
+    }
+
+
+
+    
+
+    public string Title { get; init; }
+
+    private class General
+    {
+        public string Key { get; init; }
+        public string Value { get; init; }
+    }
+
+    private class Node
+    {
+        public string Person { get; init; }
+
+        public string SoftwareSystem_Key { get; init; }
+        public string SoftwareSystem_Description { get; init; }
+
+        public string Container_Key { get; init; }
+        public string Container_Description { get; init; }
+        public string Component { get; init; }
+        public string Tier { get; init; }
+        public string Technology { get; init; }
+        public string Owner { get; init; }
+        public string Deprecated { get; init; }
+    }
+
+
+}
+
+
+
