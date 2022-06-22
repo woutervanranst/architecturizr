e157980ee03fa57ad325fdb2379515f0d32693b2
Wouter Van Ranst <wouter.vanranst@gmail.com>
2022-06-22 09:10:37 +0200

    added parsing logic

--- Program.cs
+++ Program.cs
@@ -1,6 +1,3 @@
-﻿// See https://aka.ms/new-console-template for more information
-using architecturizr;
-
-Console.WriteLine("Hello, World!");
+﻿using architecturizr;
 
 var s = new SourceFile("/Users/wouter/Documents/GitLab/solution-architecture/c4/source2.xlsx");
--- SourceFile.cs
+++ SourceFile.cs
@@ -1,5 +1,4 @@
-﻿using System;
-using ExcelToEnumerable;
+﻿using ExcelToEnumerable;
 
 namespace architecturizr;
 
@@ -7,15 +6,14 @@ namespace architecturizr;
 /// Read the Excel Source file
 /// https://stackoverflow.com/a/15793495
 /// </summary>
-public class SourceFile
+internal class SourceFile
 {
-
     public SourceFile(string fileName)
     {
         var exceptionList = new List<Exception>();
 
         // Parse General Tab
-        var generalRows = fileName.ExcelToEnumerable<General>(
+        var generalRows = fileName.ExcelToEnumerable<GeneralRow>(
             x => x
                 .UsingSheet("General")
                 .UsingHeaderNames(false) //map using column numbers, not names
@@ -29,45 +27,206 @@ public class SourceFile
 
 
         // Parse Nodes Tab
-        var nodes = fileName.ExcelToEnumerable<Node>(
+        var nodes = fileName.ExcelToEnumerable<NodeRow>(
             x => x
                 .UsingSheet("Nodes")
                 .OutputExceptionsTo(exceptionList)
-                .UsingHeaderNames(false) //map using column numbers, not names
-                .StartingFromRow(3) //data as of row 3
-            );
+                //.UsingHeaderNames(false) //map using column numbers, not names
+                .StartingFromRow(2) //data as of row 3
+            ).ToArray();
+
+        if (exceptionList.Any())
+            throw new Exception();
+
+        var bla = Parse(nodes).ToArray();
+
+        // Parse Persons
+
+        // Parse Software Systems
+        // SoftwareSystems = ParseSoftwareSystems(nodes).ToArray();
+        SoftwareSystems = bla.OfType<SoftwareSystem>().ToArray();
+
+
     }
-
-
-
-    
 
     public string Title { get; init; }
 
-    private class General
+    public IEnumerable<SoftwareSystem> SoftwareSystems { get; init; }
+
+    private IEnumerable<Node> Parse(IEnumerable<NodeRow> nodeRows)
+    {
+        var nodes = new Dictionary<string, Node>();
+
+        foreach (var row in nodeRows)
+        {
+            if (row.IsValidPersonRow)
+            {
+                // Person
+                var p = new Person(row.PersonKey);
+
+                nodes.Add(p.Key, p);
+                yield return p;
+            }
+            else if (row.IsValidSoftwareSystemRow)
+            {
+                // Software System
+                var ss = new SoftwareSystem(row.SoftwareSystemKey)
+                {
+                    Description = row.Description
+                };
+
+                nodes.Add(ss.Key, ss);
+                yield return ss;
+                
+            }
+            else if (row.IsValidContainerRow)
+            {
+                //Container
+                var parent = (SoftwareSystem)nodes[row.SoftwareSystemKey];
+                var c = new Container(parent, row.ContainerKey)
+                {
+                    Description = row.Description,
+                    Technology = row.Technology
+                };
+
+                nodes.Add(c.Key, c);
+                yield return c;
+            }
+
+
+
+            // Compontent
+        }
+    }
+
+
+    //private IEnumerable<SoftwareSystemNode> ParseSoftwareSystems(IEnumerable<NodeRow> nodes)
+    //{
+    //    var sss = nodes
+    //        .Where(n => !string.IsNullOrWhiteSpace(n.SoftwareSystemKey))
+    //        .Select(n => n.SoftwareSystemKey).Distinct();
+
+    //    foreach (var ss in sss)
+    //    {
+    //        var r = nodes.Where(n => n.SoftwareSystemKey == ss && !string.IsNullOrWhiteSpace(n.Description)).ToArray();
+
+    //        if (r.Length == 0)
+    //            throw new InvalidOperationException($"SoftwareSystem '{ss}' is not properly defined. Does not contain a Description");
+    //        else if (r.Length > 1)
+    //            throw new InvalidOperationException($"SoftwareSystem '{ss}' is not properly defined. Contains more than one Description.");
+
+    //        yield return new SoftwareSystemNode(r.Single());
+    //    }
+    //}
+
+    
+
+    private class GeneralRow
     {
         public string Key { get; init; }
         public string Value { get; init; }
     }
 
-    private class Node
+    private class NodeRow
     {
-        public string Person { get; init; }
-
-        public string SoftwareSystem_Key { get; init; }
-        public string SoftwareSystem_Description { get; init; }
-
-        public string Container_Key { get; init; }
-        public string Container_Description { get; init; }
-        public string Component { get; init; }
-        public string Tier { get; init; }
+        public string PersonKey { get; init; }
+        public string SoftwareSystemKey { get; init; }
+        public string ContainerKey { get; init; }
+        public string ComponentKey { get; init; }
         public string Technology { get; init; }
         public string Owner { get; init; }
         public string Deprecated { get; init; }
+        public string Description { get; init; }
+
+        internal bool IsValidPersonRow =>
+            !string.IsNullOrWhiteSpace(PersonKey) &&
+            string.IsNullOrWhiteSpace(SoftwareSystemKey) &&
+            string.IsNullOrWhiteSpace(ContainerKey) &&
+            string.IsNullOrWhiteSpace(ComponentKey) &&
+            string.IsNullOrWhiteSpace(Technology) &&
+            string.IsNullOrWhiteSpace(Owner) &&
+            string.IsNullOrWhiteSpace(Deprecated) &&
+            string.IsNullOrWhiteSpace(Description);
+
+        internal bool IsValidSoftwareSystemRow =>
+            string.IsNullOrWhiteSpace(PersonKey) &&
+            !string.IsNullOrWhiteSpace(SoftwareSystemKey) &&
+            string.IsNullOrWhiteSpace(ContainerKey) &&
+            string.IsNullOrWhiteSpace(ComponentKey) &&
+            string.IsNullOrWhiteSpace(Technology) &&
+            string.IsNullOrWhiteSpace(Owner) &&
+            string.IsNullOrWhiteSpace(Deprecated) &&
+            !string.IsNullOrWhiteSpace(Description);
+
+        internal bool IsValidContainerRow =>
+            string.IsNullOrWhiteSpace(PersonKey) &&
+            !string.IsNullOrWhiteSpace(SoftwareSystemKey) &&
+            !string.IsNullOrWhiteSpace(ContainerKey) &&
+            string.IsNullOrWhiteSpace(ComponentKey) &&
+            !string.IsNullOrWhiteSpace(Technology) &&
+            string.IsNullOrWhiteSpace(Owner) &&
+            string.IsNullOrWhiteSpace(Deprecated) &&
+            !string.IsNullOrWhiteSpace(Description);
+
+        internal bool IsValidComponentRow =>
+            string.IsNullOrWhiteSpace(PersonKey) &&
+            !string.IsNullOrWhiteSpace(SoftwareSystemKey) &&
+            !string.IsNullOrWhiteSpace(ContainerKey) &&
+            !string.IsNullOrWhiteSpace(ComponentKey) &&
+            !string.IsNullOrWhiteSpace(Technology) &&
+            string.IsNullOrWhiteSpace(Owner) &&
+            string.IsNullOrWhiteSpace(Deprecated) &&
+            !string.IsNullOrWhiteSpace(Description);
     }
+
+
 
 
 }
 
+internal class Node
+{
+    protected Node(string key)
+    {
+        this.Key = key;
+    }
 
+    public string Key { get; init; }
+}
+
+internal class Person : Node
+{
+    public Person(string key) : base(key) { }
+}
+
+internal class SoftwareSystem : Node
+{
+    public SoftwareSystem(string key) : base(key)
+    {
+    }
+
+    public string Description { get; init; }
+}
+
+internal class Container : Node
+{
+    public Container(SoftwareSystem parent, string key) : base(key)
+    {
+    }
+
+    public string Technology { get; init; }
+
+    public string Description { get; init; }
+}
+
+internal class Component : Node
+{
+    public Component(Container parent, string key) : base(key)
+    {
+    }
+
+    public string Technology { get; init; }
+
+    public string Description { get; init; }
+}
 
