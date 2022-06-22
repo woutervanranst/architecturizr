1ca6bcb5f1f3f11e76ee3617fe6aa0971fd3e949
Wouter Van Ranst <wouter.vanranst@gmail.com>
2022-06-22 12:07:39 +0200

    initial wire up of structurizr

--- Program.cs
+++ Program.cs
@@ -6,14 +6,18 @@ using Microsoft.Extensions.Configuration;
 // Set up on Mac & read in Console App
 //      https://makolyte.com/how-to-add-user-secrets-in-a-dotnetcore-console-app/
 //      https://dotnetcoretutorials.com/2022/04/28/using-user-secrets-configuration-in-net/
+// Add new secrets:
+//      https://developercommunity.visualstudio.com/t/manage-user-secrets/179886#T-N195679
+
+var s = new SourceFile("/Users/wouter/Documents/GitLab/solution-architecture/c4/source2.xlsx");
+
 
 var config = new ConfigurationBuilder()
     .AddUserSecrets(Assembly.GetExecutingAssembly(), true)
     .Build();
 
-
-var workspaceId = "74785";
+long workspaceId = 74785;
 var apiKey = config["structurizr:apiKey"]; // see https://structurizr.com/workspace/74785/settings
 var apiSecret = config["structurizr:apiSecret"];
 
-var s = new SourceFile("/Users/wouter/Documents/GitLab/solution-architecture/c4/source2.xlsx");
+var b = new StructurizrBuilder(s, workspaceId, apiKey, apiSecret);
---
+++ StructurizrBuilder.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using architecturizr;
+using Structurizr;
+using Structurizr.Api;
+
+namespace architecturizr
+{
+    internal class StructurizrBuilder
+    {
+        public StructurizrBuilder(SourceFile s, long workspaceId, string apiKey, string apiSecret)
+        {
+            var workspace = new Workspace(s.Title, s.Description);
+            var model = workspace.Model;
+
+            Structurizr.Person p = null;
+            foreach (var person in s.Persons)
+            {
+                p = model.AddPerson(person.Name, person.Description);
+
+            }
+
+            //var user = model.AddPerson("User", "A user of my software system.");
+            var softwareSystem = model.AddSoftwareSystem("Software System", "My software system.");
+            p.Uses(softwareSystem, "Uses");
+
+            ViewSet viewSet = workspace.Views;
+            SystemContextView contextView = viewSet.CreateSystemContextView(softwareSystem, "SystemContext", "An example of a System Context diagram.");
+            contextView.AddAllSoftwareSystems();
+            contextView.AddAllPeople();
+            contextView.AddAllElements();
+            // contextView.AddNearestNeighbours(p);
+
+            //var dv = viewSet.CreateDynamicView();
+            //dv.
+
+            // https://github.com/structurizr/dotnet-core-quickstart/blob/master/structurizr/Program.cs
+
+            // https://structurizr.com/help/themes
+            // viewSet.Configuration.Theme = "default";
+            Styles styles = viewSet.Configuration.Styles;
+            styles.Add(new ElementStyle(Tags.SoftwareSystem) { Background = "#1168bd", Color = "#ffffff" });
+            styles.Add(new ElementStyle(Tags.Person) { Background = "#08427b", Color = "#ffffff", Shape = Shape.Person });
+
+            var structurizrClient = new StructurizrClient(apiKey, apiSecret);
+            structurizrClient.PutWorkspace(workspaceId, workspace);
+        }
+    }
+}
+
