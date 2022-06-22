6682c6f61e92184807668225c918d57527f2948c
Wouter Van Ranst <wouter.vanranst@gmail.com>
2022-06-22 14:54:52 +0200

    models with Generic SaticStructureElement

---
+++ Models.cs
@@ -0,0 +1,74 @@
+﻿namespace architecturizr;
+
+internal abstract class Node
+{
+    protected Node(string key)
+    {
+        this.Key = key;
+    }
+
+    public string Key { get; init; }
+
+    public string Name { get; init; }
+
+    public string Description { get; init; }
+
+    public override string ToString() => $"{this.GetType().Name}-{Key}";
+}
+
+internal abstract class Node<T> : Node where T : Structurizr.StaticStructureElement
+{
+    protected Node(string key) : base(key)
+    {
+    }
+
+    public T StructurizrObject { get; set; }
+}
+
+internal class Person : Node<Structurizr.Person>
+{
+    public Person(string key) : base(key) { }
+}
+
+internal class SoftwareSystem : Node<Structurizr.SoftwareSystem>
+{
+    public SoftwareSystem(string key) : base(key)
+    {
+    }
+
+    public List<Container> Children { get; } = new List<Container>();
+}
+
+internal class Container : Node<Structurizr.Container>
+{
+    public Container(SoftwareSystem parent, string key) : base(key)
+    {
+        parent.Children.Add(this);
+    }
+
+    public string Technology { get; init; }
+
+    public List<Component> Children { get; } = new List<Component>();
+}
+
+internal class Component : Node<Structurizr.Component>
+{
+    public Component(Container parent, string key) : base(key)
+    {
+        parent.Children.Add(this);
+    }
+
+    public string Technology { get; init; }
+}
+
+internal class Edge
+{
+    public Edge(Node from, Node to)
+    {
+        this.From = from;
+        this.To = to;
+    }
+
+    public Node From { get; }
+    public Node To { get; } //todo remove init from all others
+}
