991e50842c0f745e272d21c83b920082a6456f41
Wouter Van Ranst <wouter.vanranst@gmail.com>
2022-12-29 15:36:23 +0100

    Create KeyNotFoundExceptionExtensions.cs

---
+++ Utils/KeyNotFoundExceptionExtensions.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace architecturizr.Utils;
+
+internal static class KeyNotFoundExceptionExtensions
+{
+    public static string GetKeyValue(this KeyNotFoundException ex)
+    {
+        var match = Regex.Match(ex.Message, @"'(.*?)'");
+        if (match.Success)
+            return match.Groups[1].Value;
+
+        throw ex;
+    }
+}
