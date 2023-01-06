dfa732f7c4213f428df97f98b11e8f7030e4306d
Wouter Van Ranst <wouter.vanranst@gmail.com>
2023-01-06 17:16:52 +0100

    updated async regex

--- InputParsers/PlantUmlParser.cs
+++ InputParsers/PlantUmlParser.cs
@@ -30,10 +30,10 @@ internal partial class PlantUmlParser : IINputParser<Process>
     [GeneratedRegex(@"(?<from>[\w-]*) ?-> ?(?<to>[\w-]*) ?: ?\ ?(?<description>.*)")]
     private static partial Regex SyncStepRegex();
     
-    [GeneratedRegex(@"(?<from>[\w-]*) [-]?->\([0-9]\) (?<to>[\w-]*) ?: ?\[(?<topic>[\w-]*)\] ?(?<description>.*)")]
+    [GeneratedRegex(@"(?<from>[\w-]*) [-]?->\([0-9]\) (?<to>[\w-]*) ?: ?\[(?<topic>[\w-\.]*)\] ?(?<description>.*)?")]
     private static partial Regex AsyncStepRegex();
 
-    [GeneratedRegex(@"(?<to>[\w-]*) ?<-- ?(?<from>[\w-]*) ?: ?\ ?(?<description>.*)")]
+    [GeneratedRegex(@"(?<to>[\w-]*) ?<-- ?(?<from>[\w-\.]*) ?: ?\ ?(?<description>.*)?")]
     private static partial Regex SyncReturnRegex();
 
     [GeneratedRegex(@"==\s*(?<sectionName>.*?)\s*==")]
