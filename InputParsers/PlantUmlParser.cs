863874d5136aa5cf0b4ed4aaa0c3b39e6a83401b
Wouter Van Ranst <wouter.vanranst@gmail.com>
2023-01-07 23:16:05 +0100

    feat(plantuml parser) improved error handling

--- InputParsers/PlantUmlParser.cs
+++ InputParsers/PlantUmlParser.cs
@@ -60,6 +60,18 @@ internal partial class PlantUmlParser : IINputParser<Process>
                 p.FullName = r0.Value;
                 title = r0.Value;
             }
+            else if (SectionRegex().Match(line) is { Success: true } r1)
+            {
+                // Flowchart section
+                if (p.Steps.Count != 0)
+                    yield return p;
+
+                p = new Process(fi)
+                {
+                    Name = title,
+                    FullName = $"{title} - {r1.Groups["sectionName"].Value}"
+                };
+            }
             else if (line.StartsWith("#") || line.StartsWith("'"))
             {
                 // Comment line -- ignore
@@ -79,38 +91,37 @@ internal partial class PlantUmlParser : IINputParser<Process>
             {
                 // various control statements -- ignore
             }
-            else if (SectionRegex().Match(line) is { Success: true} r1)
-            {
-                // Flowchart section
-                if (p.Steps.Count != 0)
-                    yield return p;
-                
-                p = new Process(fi)
-                {
-                    Name = title,
-                    FullName = $"{title} - {r1.Groups["sectionName"].Value}"
-                };
-            }
-            else if (SyncStepRegex().Match(line) is { Success: true } r2)
-            {
-                // Sync step
-                var s = ParseSyncStep(r2);
-                p.Steps.Add(s);
-            }
-            else if (SyncReturnRegex().Match(line) is { Success: true } r3)
-            {
-                // A Sync Return Step -- ignore this for now, this makes the diagram confusing
-                //var s = ParseSyncStep(r3);
-                //p.Steps.Add(s);
-            }
-            else if (AsyncStepRegex().Match(line) is { Success: true } r4)
-            {
-                // Async step
-                var s = ParseAsyncStep(r4);
-                p.Steps.Add(s);
-            }
             else
-                throw new Exception($"{fi.Name}: Error on line {i}: '{line}' cannot be parsed");
+            {
+                // this weird construct since we can't use yield in a try/catch block
+                try
+                {
+                    if (SyncStepRegex().Match(line) is { Success: true } r2)
+                    {
+                        // Sync step
+                        var s = ParseSyncStep(r2);
+                        p.Steps.Add(s);
+                    }
+                    else if (SyncReturnRegex().Match(line) is { Success: true } r3)
+                    {
+                        // A Sync Return Step -- ignore this for now, this makes the diagram confusing
+                        //var s = ParseSyncStep(r3);
+                        //p.Steps.Add(s);
+                    }
+                    else if (AsyncStepRegex().Match(line) is { Success: true } r4)
+                    {
+                        // Async step
+                        var s = ParseAsyncStep(r4);
+                        p.Steps.Add(s);
+                    }
+                    else
+                        throw new InvalidOperationException($"'{fi.Name}' line {i}: '{line}' did not match any regular expression");
+                }
+                catch (Exception e)
+                {
+                    throw new Exception($"'{fi.Name}' line {i}: '{line}' cannot be parsed", e);
+                }
+            }
         }
 
         yield return p;
